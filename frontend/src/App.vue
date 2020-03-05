be578c41c65bb21e6473f1f27897bcdcdf3aef3f
Aidan Smith <lolbot55555@gmail.com>
2020-03-05 12:10:32 -0800

    Sidebar CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,47 +6,78 @@
           <source src="https://assets14.ign.com/videos/zencoder/2020/02/20/416/b7422245c865698feee05a1f809eeb4c-110000-1582191191.mp4" type="video/mp4">        <!-- fallback content here -->
         </video>
         <div class="columns">
-        <b class="header column">World's Coolest Lorem Ipsum</b>
+          <b class="title is-size-1 column">World's Coolest Lorem Ipsum</b>
         </div>
         <div class="columns">
-          <div class="body column">Look at all of this great, fascinating text that I'm going to read right now. I sure do love reading all of this text.</div>
+          <div class="body column">Look at all of this great, fascinating text that I'm goingok at all of this great, fascinating text that I'm goingok at all of this great, fascinating text that I'm goingok at all of this great, fascinating text that I'm goingok at all of this great, fascinating text that I'm goingok at all of this great, fascinating text that I'm going to read right now. I sure do love reading all of this text.</div>
         </div>
-      </div>
-      <div class="column">Eventually, playlist cards.</div>
+     </div>
+    <div class="column playlist is-4">
+      <UpcomingPlaylist/>
     </div>
+    </div>
+  
+   
   </div>
 </template>
 
 <script>
 import './../node_modules/bulma/css/bulma.css';
-
+import UpcomingPlaylist from './components/UpcomingPlaylist';
 export default {
   name: 'App',
   components: {
+    UpcomingPlaylist
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:  'Open Sans', 'Helvetica Neue', sans-serif, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10vh;
+
 }
 video {
   width:130vh;
 }
-.header {
-  text-align: left;
-  font-size:4em;
-}
+
 .body {
-  text-align: left;
-  font-size:3em;
-  text-indent:3em;
-  margin-top:-.5em;
+  font-size:1.5em;
+  margin-top:-.2em;
+  line-height:1.4;
 }
+.title {
+  margin-top:.75em;
+  font-family:  'Open Sans', 'Helvetica Neue', sans-serif, sans-serif;
+  -webkit-text-stroke-width: .1vh;
+  -webkit-text-stroke-color: rgb(#363636);
+
+}
+
+/* Small devices (landscape phones, 576px and up) */
+  @media (min-width: 576px) {  }
+    #app {
+            margin-left:0;
+          }
+   /* Medium devices (tablets, 768px and up) */
+  @media (min-width: 768px) { 
+
+   }
+
+   /* Large devices (desktops, 992px and up) */
+  @media (min-width: 992px) {  
+    #app {
+                margin-left:10vw;
+              }
+  }
+
+   /* Extra large devices (large desktops, 1200px and up) */
+  @media (min-width: 1200px) { 
+    
+   }
 </style>
---
+++ frontend/src/components/UpcomingPlaylist.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class=" root">
+        <div v-for="val in Array(4).keys()" :key="val">
+            <div class="columns">
+                <div class="column card-image" style="background-image:">   
+                    <div class="card-time">3:21</div>
+                    <img class="card-image" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpbs.bento.storage.s3.amazonaws.com%2Fhostedbento-prod%2Ffiler_public%2FBig%2520Pacific%2FPhotos%2FBigPacific_Promo_Thumb.jpg&f=1&nofb=1"/>
+                </div>
+                <div class="column playlist-header">
+                    This is my attempt at writing a lot of example text.
+                </div>
+            </div>
+            <div class="columns"> 
+                <hr :v-if="val!=3"/>    
+            </div>
+        </div>
+        <button class="load-btn"> <b>Load More</b></button>
+    </div>
+</template>
+
+<script>
+import './../../node_modules/bulma/css/bulma.css';
+export default {
+    'name':'UpcomingPlaylist',
+    props: {
+        cards: [],
+        index: Number,
+    },
+}
+</script>
+
+<style scoped>
+hr {
+    margin-top:2.5%;
+    height:.25vh;
+    width:110%;
+    margin-left:2%;
+    margin-bottom:5%;
+    background-color:lightgray;
+}
+.root {
+    position: sticky;
+    top:0;
+}
+.card-image {
+    border-radius: .5vw;
+}
+.playlist-header {
+    font-size:1.2em;
+    -webkit-text-stroke-width: .03vh;
+    -webkit-text-stroke-color: #363636;
+    margin-top:.25em;
+    line-height: 1.3;
+}
+.load-btn {
+    background-color: #BF1313;
+    width:103%;
+    height:2.7em;
+    color:white;
+    border-radius:.4vw;
+    border:none;
+    font-size:1.15em;
+    -webkit-text-stroke-width: .06vh;
+    -webkit-text-stroke-color: white;
+    outline: none;
+}
+.load-btn :after {
+    background-color: rgba(191, 19, 19, 0.876);
+}
+.card-time {
+    position: absolute;
+    z-index: 99;
+    color:white;
+    bottom:15%;
+    left:75%;
+    font-size:.8em;
+}
+</style>
