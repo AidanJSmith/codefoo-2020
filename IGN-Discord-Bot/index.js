2fbfa2726a1aad792c00736e5b07c74636321417
Aidan Smith <lolbot55555@gmail.com>
2020-03-16 11:48:15 -0700

    Bot v1.1

--- IGN-Discord-Bot/index.js
+++ IGN-Discord-Bot/index.js
@@ -1,84 +1,178 @@
 const Discord = require('discord.js');
-const {prefix, token} = require('./credentials.json');
+const {
+    prefix,
+    token
+} = require('./credentials.json');
 var Xray = require('x-ray')
 var x = Xray()
 const client = new Discord.Client();
 
-function applyFilter(query,filter="all") {
+function applyFilter(query, filter = "all") {
     var url = new URL("https://www.ign.com/search");
-    url.searchParams.set('q',query);
-    url.searchParams.set('count',1);
-    url.searchParams.set('filter',filter);    
-    url.searchParams.set('type',filter);    
-    return url;  
+    url.searchParams.set('q', query);
+    url.searchParams.set('count', 10);
+    url.searchParams.set('filter', filter);
+    url.searchParams.set('type', filter);
+    return url;
+}
+
+function reactAwaitEdit(originalmessage, message, place, max) {
+    message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+    const filter = (reaction, user) => {
+        return ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id != message.author.id;
+    };
+
+    if (place == 1) {
+        message.react(`➡️`).then(message.awaitReactions(filter, {
+                max: 1,
+                time: 60000,
+                errors: ['time']
+            })
+            .then(collected => {
+                const reaction = collected.first();
+                if (reaction.emoji.name === '➡️') {
+                    wikiEmbed(originalmessage,place+1,"edit",message);
+                } else {
+                    message.channel.send('you reacted with left');
+                }
+            })
+            .catch(collected => {
+                message.reply(`you reacted with neither a thumbs up, nor a thumbs down (${collected[0]})`);
+            }));
+    } else {
+        if (max > 1) {
+            message.react('⬅️').then(max != place ? message.react('➡️') : '').then(message.awaitReactions(filter, {
+                max: 1,
+                time: 60000,
+                errors: ['time']
+            })
+            .then(collected => {
+                const reaction = collected.first();
+                if (reaction.emoji.name === '➡️') {
+                    wikiEmbed(originalmessage,place+1,"edit",message);
+                } else {
+                    wikiEmbed(originalmessage,place-1,"edit",message);
+                }
+            })
+            .catch(collected => {
+                console.log(`Reaction: (${collected[0]})`);
+            }));
+        }
+    }
+
+}
+
+function wikiEmbed(message, place = 1, mode = "default", editmessage = "null") {
+    x(applyFilter(message.content.slice(8), "wiki").toString(), {
+        props: x('.search-item', [{
+            title: x(".search-item-title", {
+                text: "a",
+                url: "a@href",
+            }),
+            subtitle: x(".search-item-sub-title", {
+                link: "a@href",
+                text: 'a',
+            }),
+            description: ".search-item-description"
+        }])
+    })(function (err, obj) {
+        const max = obj.props.length;
+        obj = {
+            props: obj.props[place - 1]
+        };
+        if (obj.props.title.url != undefined) {
+            obj.props.title.url = obj.props.title.url.replace(/ /g, '_');
+            if (mode == "edit") {
+                x(obj.props.title.url, ".image@src").then(function (q) {
+                    let url = new URL(`https://` + new URL(q).hostname + new URL(q).pathname).toString();
+                    const embed = new Discord.MessageEmbed()
+                        .setColor('#D3222A')
+                        .setTitle(obj.props.title.text)
+                        .setURL(obj.props.title.url)
+                        .setDescription(obj.props.description)
+                        .setImage(url)
+                        .addFields({
+                            name: 'Wiki',
+                            value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`
+                        }, )
+                        .setFooter('Bot by Aidan Smith. v1.1')
+                    editmessage.edit(embed);
+                    reactAwaitEdit(message, editmessage, place, max);
+                }).catch(function () {
+                    console.log("No image found");
+                    const embed = new Discord.MessageEmbed()
+                        .setColor('#D3222A')
+                        .setTitle(obj.props.title.text)
+                        .setURL(obj.props.title.url)
+                        .setDescription(obj.props.description)
+                        .addFields({
+                            name: 'Wiki',
+                            value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`
+                        }, )
+                        .setFooter('Bot by Aidan Smith. v1.1')
+                    editmessage.edit(embed);
+                    reactAwaitEdit(message, editmessage, place, max);
+                })
+            }
+            if (mode == "default") {
+            x(obj.props.title.url, ".image@src").then(function (q) {
+                    let url = new URL(`https://` + new URL(q).hostname + new URL(q).pathname).toString();
+                    const embed = new Discord.MessageEmbed()
+                        .setColor('#D3222A')
+                        .setTitle(obj.props.title.text)
+                        .setURL(obj.props.title.url)
+                        .setDescription(obj.props.description)
+                        .setImage(url)
+                        .addFields({
+                            name: 'Wiki',
+                            value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`
+                        }, )
+                        .setFooter('Bot by Aidan Smith. v1.1')
+                    message.channel.send(embed).then(function () {
+                        while (message.channel.lastMessage == message) {
+                            console.log("");
+                        }
+                        reactAwaitEdit(message,message.channel.lastMessage, place, max);
+                    });
+                
+            }).catch(function () {
+                console.log("No image found");
+                if (mode == "default") {
+                    message.channel.send(new Discord.MessageEmbed()
+                        .setColor('#D3222A')
+                        .setTitle(obj.props.title.text)
+                        .setURL(obj.props.title.url)
+                        .setDescription(obj.props.description)
+                        .addFields({
+                            name: 'Wiki',
+                            value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`
+                        }, )
+                        .setFooter('Bot by Aidan Smith. v1.1')).then(function () {
+                            while (message.channel.lastMessage == message) {
+                                console.log("");
+                            }
+                            reactAwaitEdit(message,message.channel.lastMessage, place, max);
+                        });
+                }
+            })}
+        } else {
+            message.channel.send("Query not found.");
+        }
+
+
+    })
 }
 client.once('ready', () => {
-	console.log('Active.');
+    console.log('Active.');
 });
 client.on('message', message => {
     console.log(message.content);
     if (message.content.startsWith(`${prefix}`)) {
-        // send back "Pong." to the channel the message was sent in
         if (message.content.startsWith(`${prefix} wiki`)) {
-           x(applyFilter(message.content.slice(8),"wiki").toString(), {
-                props:x('.search-item',{
-                    title: x(".search-item-title", {
-                        text:"a",
-                        url:"a@href",
-                    }),
-                    subtitle: x(".search-item-sub-title",{
-                        link:"a@href",
-                        text:'a',
-                    }),
-                    description:".search-item-description"
-                })
-            })(function(err, obj) { 
-                if (obj.title.url!=undefined) {
-                    obj.props.title.url=obj.props.title.url.replace(/ /g,'_');
-                    x(obj.props.title.url,".image@src").then(function(q) {
-                            let url =  new URL(`https://`+new URL(q).hostname+new URL(q).pathname).toString();
-                            
-                            message.channel.send(new Discord.MessageEmbed()
-                            .setColor('#D3222A')
-                            .setTitle(obj.props.title.text)
-                            .setURL(obj.props.title.url)
-                            .setDescription(obj.props.description)
-                            .setImage(url)
-                            .addFields(
-                                { name: 'Wiki', value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`},
-                            )
-                            .setFooter('Bot by Aidan Smith. V1')) 
-                        
-                    }).catch(function(err) {
-                        console.log(err==false ? "No Image Found" : err);
-                        message.channel.send(new Discord.MessageEmbed()
-                                .setColor('#D3222A')
-                                .setTitle(obj.props.title.text)
-                                .setURL(obj.props.title.url)
-                                .setDescription(obj.props.description)
-                                .addFields(
-                                    { name: 'Wiki', value: `[${obj.props.subtitle.text}](${obj.props.subtitle.link})`},
-                                )
-                                .setFooter('Bot by Aidan Smith. V1'))
-                    }) } else {
-                        message.channel.send("Query not found.");
-                    }
-
-                
-            })
-             /*
-              x(content,'search-item-sub-title')(function(err, callback) {
-                return callback;
-            });
-             x(content,'search-item-description')(function(err, callback) {
-                return callback;
-            });
-            */
+            wikiEmbed(message);
         } else if (message.content.startsWith(`${prefix} video `)) {
-            // send back "Pong." to the channel the message was sent in
             message.channel.send('Just search for video');
         } else if (message.content.startsWith(`${prefix} game`)) {
-            // send back "Pong." to the channel the message was sent in
             message.channel.send('Just search the games');
         } else {
             message.channel.send('Make a sitewide query');
@@ -86,7 +180,7 @@ client.on('message', message => {
 
 
     }
-    
+
 });
 
 client.login(token);
